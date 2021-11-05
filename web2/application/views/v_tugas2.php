c12320e9433d0314871c5df6abb66d05b573a2fc
herryono <herryono.sct@gmail.com>
2021-11-05 14:32:44 +0800

    adding files

---
+++ web2/application/views/v_tugas2.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="<?=base_url('assets/css/bootstrap.css') ?>">
+	<script src="<?=base_url('assets/js/bootstrap.css')?>"></script>
+</head>
+<body>
+<div class="alert alert-primary" style="text-align: center;" role="alert">
+  <h3>Tugas 2</h3>
+</div>
+
+
+<div class="container">
+  <div class="row">
+    <div class="col-6">
+      	<table class="table">
+		  	<thead>
+			    <tr>
+			      
+			      <th scope="col">NIM</th>
+			      <th scope="col">NAMA</th>
+			      <th scope="col">ALAMAT</th>
+			    </tr>
+		  	</thead>
+			<tbody>
+			    <tr>
+			      
+			      <td>2101040015</td>
+			      <td>Herryono Soegiarto</td>
+			      <td>Cakranegara</td>
+			    </tr>
+  
+  		</tbody>
+		</table>
+    </div>
+    <div class="col-3">
+    	<div class="card" style="width: 18rem;">
+		  <img class="card-img-top" src="<?=base_url('assets/img/pp.jpg') ?>" alt="Card image cap">
+		  <div class="card-body" style="text-align: center;" >
+		  	<p class="font-weight-bold">FOTO</p>
+		    
+		  </div>
+		</div>
+     <!--  <div class="p-2" style="border: solid 1px;">
+      	<img width="120" height="100" src="<?=base_url('assets/img/pp.jpg') ?>">
+      </div> -->
+    </div>
+     <div class="col-3">
+      
+    </div>
+  </div>
+
+</div>
+
+
+
+
+
+</body>
+</html>
